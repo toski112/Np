ca9e5dfc7f5d5dd180fbacd67a7ad519f6d28a4c
Daniel Gutowski <gutowiaczek@gmail.com>
2017-07-06 23:27:33 +0200

    0.0.0

---
+++ Np.java
@@ -0,0 +1,11 @@
+
+public class Np {
+    public static void main(String[] args) {
+
+        System.out.println("tworzenie tab recznie");
+        int[] tab = new int[30];
+        tab[0] = 1;
+        
+
+    }
+}
